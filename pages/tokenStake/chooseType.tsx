b6564ba167c1fece61429de8f96d66d764fea3e6
ravirasadiya <ravirasadiya1994@gmail.com>
2024-08-23 09:59:48 +0530

    fix: update text

--- pages/tokenStake/chooseType.tsx
+++ pages/tokenStake/chooseType.tsx
@@ -1,19 +1,19 @@
-import classNames from "classnames";
-import { CardContainer } from "components/common/CardContainer";
-import { CustomButton } from "components/common/CustomButton";
-import { ChooseTypeGuide } from "components/tokenStake/ChooseTypeGuide";
-import { robotoBold } from "config/font";
-import { useAppDispatch, useAppSelector } from "hooks/common";
-import { useIsTrustedValidator } from "hooks/useIsTrustedValidator";
-import { useSoloDepositEnabled } from "hooks/useSoloDepositEnabled";
-import { useSoloNodeDepositAmount } from "hooks/useSoloNodeDepositAmount";
-import { useTrustDepositEnabled } from "hooks/useTrustDepositEnabled";
-import { useWalletAccount } from "hooks/useWalletAccount";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { RootState } from "redux/store";
-import { openLink } from "utils/commonUtils";
-import { getTokenName } from "utils/configUtils";
+import classNames from 'classnames';
+import { CardContainer } from 'components/common/CardContainer';
+import { CustomButton } from 'components/common/CustomButton';
+import { ChooseTypeGuide } from 'components/tokenStake/ChooseTypeGuide';
+import { robotoBold } from 'config/font';
+import { useAppDispatch, useAppSelector } from 'hooks/common';
+import { useIsTrustedValidator } from 'hooks/useIsTrustedValidator';
+import { useSoloDepositEnabled } from 'hooks/useSoloDepositEnabled';
+import { useSoloNodeDepositAmount } from 'hooks/useSoloNodeDepositAmount';
+import { useTrustDepositEnabled } from 'hooks/useTrustDepositEnabled';
+import { useWalletAccount } from 'hooks/useWalletAccount';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { RootState } from 'redux/store';
+import { openLink } from 'utils/commonUtils';
+import { getTokenName } from 'utils/configUtils';
 
 const ChooseTypePage = () => {
   const router = useRouter();
@@ -34,21 +34,21 @@ const ChooseTypePage = () => {
     !metaMaskAccount;
 
   return (
-    <div className="w-smallContentW xl:w-contentW 2xl:w-largeContentW mx-auto">
-      <div className="flex mt-[.24rem] items-start">
-        <CardContainer width="6.2rem" title="Choose Validator Type">
+    <div className='w-smallContentW xl:w-contentW 2xl:w-largeContentW mx-auto'>
+      <div className='flex mt-[.24rem] items-start'>
+        <CardContainer width='6.2rem' title='Choose Validator Type'>
           <div
-            className="pb-[.52rem] flex justify-center pt-[.52rem] flex-wrap gap-[.12rem]"
+            className='pb-[.52rem] flex justify-center pt-[.52rem] flex-wrap gap-[.12rem]'
             style={{
-              rowGap: ".32rem",
-              columnGap: ".12rem",
+              rowGap: '.32rem',
+              columnGap: '.12rem',
             }}
           >
-            <div className="w-[1.88rem] h-[2.11rem] rounded-[.16rem] bg-color-bgPage flex flex-col items-center justify-between">
-              <div className="mt-[.32rem] flex flex-col items-center">
+            <div className='w-[1.88rem] h-[2.11rem] rounded-[.16rem] bg-color-bgPage flex flex-col items-center justify-between'>
+              <div className='mt-[.32rem] flex flex-col items-center'>
                 <div
                   className={classNames(
-                    "text-[.16rem] text-color-text1",
+                    'text-[.16rem] text-color-text1',
                     robotoBold.className
                   )}
                 >
@@ -57,19 +57,19 @@ const ChooseTypePage = () => {
 
                 <div
                   className={classNames(
-                    "text-[.14rem] text-color-text2 mt-[.14rem] text-center mx-[.12rem] leading-snug"
+                    'text-[.14rem] text-color-text2 mt-[.14rem] text-center mx-[.12rem] leading-snug'
                   )}
                 >
                   Deposit {getTokenName()} to be delegated
                 </div>
               </div>
 
-              <div className="self-stretch mb-[.24rem] mx-[.16rem]">
+              <div className='self-stretch mb-[.24rem] mx-[.16rem]'>
                 <CustomButton
-                  height=".48rem"
+                  height='.48rem'
                   disabled={soloDisabled}
                   onClick={() => {
-                    router.push("/tokenStake/soloDeposit");
+                    router.push('/tokenStake/soloDeposit');
                   }}
                 >
                   Next
@@ -77,11 +77,11 @@ const ChooseTypePage = () => {
               </div>
             </div>
 
-            <div className="w-[1.88rem] h-[2.11rem] rounded-[.16rem] bg-color-bgPage flex flex-col items-center justify-between">
-              <div className="mt-[.32rem] flex flex-col items-center">
+            <div className='w-[1.88rem] h-[2.11rem] rounded-[.16rem] bg-color-bgPage flex flex-col items-center justify-between'>
+              <div className='mt-[.32rem] flex flex-col items-center'>
                 <div
                   className={classNames(
-                    "text-[.16rem] text-color-text1",
+                    'text-[.16rem] text-color-text1',
                     robotoBold.className
                   )}
                 >
@@ -90,30 +90,30 @@ const ChooseTypePage = () => {
 
                 <div
                   className={classNames(
-                    "text-[.14rem] text-color-text2 mt-[.14rem] mx-[.12rem] leading-snug"
+                    'text-[.14rem] text-color-text2 mt-[.14rem] mx-[.12rem] leading-snug'
                   )}
                 >
                   Apply to be nominated
                 </div>
               </div>
 
-              <div className="self-stretch mb-[.24rem] mx-[.16rem]">
+              <div className='self-stretch mb-[.24rem] mx-[.16rem]'>
                 {!isTrust ? (
                   <CustomButton
-                    height=".48rem"
-                    type="stroke"
+                    height='.48rem'
+                    type='stroke'
                     disabled={!trustDepositEnabled}
                     onClick={() => {
-                      openLink("https://forms.gle/RtFK7qo9GzabQTCfA");
+                      openLink('https://forms.gle/RtFK7qo9GzabQTCfA');
                     }}
                   >
                     Apply
                   </CustomButton>
                 ) : (
                   <CustomButton
-                    height=".48rem"
+                    height='.48rem'
                     onClick={() => {
-                      router.push("/tokenStake/trustDeposit");
+                      router.push('/tokenStake/trustDeposit');
                     }}
                   >
                     Next
@@ -122,11 +122,11 @@ const ChooseTypePage = () => {
               </div>
             </div>
 
-            <div className="w-[1.88rem] h-[2.11rem] rounded-[.16rem] bg-color-bgPage flex flex-col items-center justify-between">
-              <div className="mt-[.32rem] flex flex-col items-center mx-[.16rem]">
+            <div className='w-[1.88rem] h-[2.11rem] rounded-[.16rem] bg-color-bgPage flex flex-col items-center justify-between'>
+              <div className='mt-[.32rem] flex flex-col items-center mx-[.16rem]'>
                 <div
                   className={classNames(
-                    "text-[.16rem] text-color-text1",
+                    'text-[.16rem] text-color-text1',
                     robotoBold.className
                   )}
                 >
@@ -135,19 +135,19 @@ const ChooseTypePage = () => {
 
                 <div
                   className={classNames(
-                    "text-[.14rem] text-color-text2 mt-[.14rem] leading-snug text-center mx-[.12rem]"
+                    'text-[.14rem] text-color-text2 mt-[.14rem] leading-snug text-center mx-[.12rem]'
                   )}
                 >
-                  Use 3rd Party Server, such as SSV and Obol
+                  Not Yet Available on Pulsechain
                 </div>
               </div>
 
-              <div className="self-stretch mb-[.24rem] mx-[.16rem]">
+              <div className='self-stretch mb-[.24rem] mx-[.16rem]'>
                 <CustomButton
-                  height=".48rem"
-                  type="stroke"
+                  height='.48rem'
+                  type='stroke'
                   onClick={() => {
-                    openLink("https://docs.stafi.io/dvt/#ssv-integration");
+                    openLink('https://docs.stafi.io/dvt/#ssv-integration');
                   }}
                 >
                   Instruction
@@ -157,7 +157,7 @@ const ChooseTypePage = () => {
           </div>
         </CardContainer>
 
-        <div className="ml-[.85rem]">
+        <div className='ml-[.85rem]'>
           <ChooseTypeGuide />
         </div>
       </div>
