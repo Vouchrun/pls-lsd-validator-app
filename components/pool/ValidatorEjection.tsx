306d7ba1f5a0435cfabcd6b222772742ca1c3cd2
ravirasadiya <ravirasadiya1994@gmail.com>
2024-12-19 18:18:21 +0530

    fix: udpate

--- components/pool/ValidatorEjection.tsx
+++ components/pool/ValidatorEjection.tsx
@@ -1,3 +1,4 @@
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import classNames from 'classnames';
 import { EmptyContent } from 'components/common/EmptyContent';
 import { Icomoon } from 'components/icon/Icomoon';
@@ -9,15 +10,30 @@ import {
   getNetworkWithdrawContractAbi,
   getNodeDepositContractAbi,
 } from 'config/contractAbi';
-import { getBeaconHost, getWithdrawContractDeploymentBlock } from 'config/env';
+import {
+  getBeaconHost,
+  getValidatorInfoURL,
+  getWithdrawContractDeploymentBlock,
+} from 'config/env';
 import { robotoBold, robotoSemiBold } from 'config/font';
 import { useAppSlice } from 'hooks/selector';
 import * as moment from 'moment';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { openLink } from 'utils/commonUtils';
 import { getDocLinks } from 'utils/configUtils';
 import { getShortAddress } from 'utils/stringUtils';
 import { getEthWeb3 } from 'utils/web3Utils';
+
+const filterOptions = [
+  {
+    label: 'All Type',
+    value: 'all',
+  },
+  { label: 'Exited', value: 'Exited' },
+  { label: 'Pending', value: 'Pending' },
+  { label: 'Delayed', value: 'Delayed' },
+];
 
 const findStatus = (status: string) => {
   if (status === 'withdrawal_done') {
@@ -25,7 +41,7 @@ const findStatus = (status: string) => {
   } else if (status === 'active_exiting') {
     return 'Pending';
   } else if (status === 'active_ongoing') {
-    return 'Missed';
+    return 'Delayed';
   } else {
     return 'Unknown';
   }
@@ -44,6 +60,8 @@ const findStatusSymbol = (status: string) => {
 };
 export const ValidatorEjection = () => {
   const { darkMode } = useAppSlice();
+  const router = useRouter();
+  const [selectedFilter, setSelectedFilter] = useState('all');
   const [validatorElectionData, setValidatorElectionData] = useState<any>([]);
 
   const web3 = getEthWeb3();
@@ -69,7 +87,6 @@ export const ValidatorEjection = () => {
         toBlock: currentBlock,
       }
     );
-    console.log(events);
     events.forEach(async (event: any) => {
       const block = await web3.eth.getBlock(event.blockNumber);
       const timeStamp = block.timestamp;
@@ -113,36 +130,51 @@ export const ValidatorEjection = () => {
   }, []);
   return (
     <div>
-      <div className='mt-[.48rem] flex items-center'>
-        <div
-          className={classNames(
-            robotoBold.className,
-            'text-[.24rem] text-color-text1'
-          )}
-        >
-          Validator Election
+      <div className='mt-[.48rem] flex items-center justify-between'>
+        <div className='flex items-center'>
+          <div
+            className={classNames(
+              robotoBold.className,
+              'text-[.24rem] text-color-text1'
+            )}
+          >
+            Validator Election
+          </div>
+
+          <div
+            className={classNames(
+              'ml-[.16rem] items-center cursor-pointer',
+              getDocLinks().ejectionMechanism ? 'flex' : 'hidden'
+            )}
+            onClick={() => {
+              openLink(getDocLinks().ejectionMechanism);
+            }}
+          >
+            <div className='text-color-text2 text-[.16rem]'>
+              Ejection Mechanism
+            </div>
+
+            <div className='ml-[.06rem] flex items-center'>
+              <Icomoon
+                icon='right'
+                size='.1rem'
+                color={darkMode ? '#ffffff80' : '#6C86AD'}
+              />
+            </div>
+          </div>
         </div>
-
-        <div
-          className={classNames(
-            'ml-[.16rem] items-center cursor-pointer',
-            getDocLinks().ejectionMechanism ? 'flex' : 'hidden'
-          )}
-          onClick={() => {
-            openLink(getDocLinks().ejectionMechanism);
-          }}
-        >
-          <div className='text-color-text2 text-[.16rem]'>
-            Ejection Mechanism
-          </div>
-
-          <div className='ml-[.06rem] flex items-center'>
-            <Icomoon
-              icon='right'
-              size='.1rem'
-              color={darkMode ? '#ffffff80' : '#6C86AD'}
-            />
-          </div>
+        <div>
+          <select
+            value={selectedFilter}
+            onChange={(e) => {
+              setSelectedFilter(e.target.value);
+            }}
+            className='bg-transparent border-[1px] border-color-border1 rounded-[.3rem] px-[.24rem] py-[.1rem] text-[.16rem] text-color-text2 w-[1.5rem] outline-none'
+          >
+            {filterOptions.map((filter) => (
+              <option value={filter.value}>{filter.label}</option>
+            ))}
+          </select>
         </div>
       </div>
 
@@ -162,11 +194,11 @@ export const ValidatorEjection = () => {
           </div>
 
           <div className='flex items-center justify-center text-[.16rem] text-color-text2'>
-            Health
+            Exited
           </div>
 
           <div className='flex items-center justify-center text-[.16rem] text-color-text2'>
-            Chosen Time (UTC)
+            Election Time (UTC)
           </div>
 
           <div className='flex items-center justify-center text-[.16rem] text-color-text2'>
@@ -182,50 +214,76 @@ export const ValidatorEjection = () => {
 
         {validatorElectionData.length > 0 && (
           <div className='max-h-[4.2rem] overflow-auto'>
-            {validatorElectionData.map((item: any, index: number) => (
-              <div
-                key={index}
-                className={classNames(
-                  'h-[.74rem] grid items-center font-[500]',
-                  index % 2 === 0 ? 'bg-bgPage/50 dark:bg-bgPageDark/50' : ''
-                )}
-                style={{
-                  gridTemplateColumns: '20% 20% 20% 20% 20%',
-                }}
-              >
-                <div className='flex items-center justify-center text-[.16rem] text-color-text2 cursor-pointer'>
-                  <div className='flex items-center'>
-                    <div className='mx-[.06rem]'>
-                      {getShortAddress(item.nodeAddress, 4)}
+            {validatorElectionData.filter(
+              (item: any) =>
+                selectedFilter === 'all' || item.status === selectedFilter
+            ).length === 0 ? (
+              <div className='h-[2rem] flex items-center justify-center'>
+                <EmptyContent />
+              </div>
+            ) : (
+              validatorElectionData
+                .filter(
+                  (item: any) =>
+                    selectedFilter === 'all' || item.status === selectedFilter
+                )
+                .map((item: any, index: number) => (
+                  <div
+                    key={index}
+                    className={classNames(
+                      'h-[.74rem] grid items-center font-[500]',
+                      index % 2 === 0
+                        ? 'bg-bgPage/50 dark:bg-bgPageDark/50'
+                        : ''
+                    )}
+                    style={{
+                      gridTemplateColumns: '20% 20% 20% 20% 20%',
+                    }}
+                  >
+                    <div className='flex items-center justify-center text-[.16rem] text-color-text2 cursor-pointer'>
+                      <div className='flex items-center'>
+                        <div
+                          className='mx-[.06rem]'
+                          onClick={() => {
+                            router.push(`/pubkey/${item.nodeAddress}`);
+                          }}
+                        >
+                          {getShortAddress(item.nodeAddress, 4)}
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className='flex items-center justify-center text-[.16rem] text-color-text2 cursor-pointer'>
+                      <div className='mx-[.06rem]'>
+                        <a
+                          href={
+                            getValidatorInfoURL() +
+                            'validator/' +
+                            item.poolAddress
+                          }
+                          target='_blank'
+                        >
+                          {getShortAddress(item.poolAddress, 4)}
+                        </a>
+                      </div>
+                    </div>
+
+                    <div className='flex items-center justify-center text-[.16rem] cursor-pointer'>
+                      <div className='mx-[.06rem]'>{item.statusSymbol}</div>
+                    </div>
+
+                    <div className='flex items-center justify-center text-[.16rem] text-color-text2 cursor-pointer'>
+                      <div className='mx-[.06rem]'>
+                        {moment.utc(item.timeStamp).format('D MMM YYYY h:mm a')}
+                      </div>
+                    </div>
+
+                    <div className='flex items-center justify-center text-[.16rem] text-color-text2 cursor-pointer'>
+                      <div className='mx-[.06rem]'>{item.status}</div>
                     </div>
                   </div>
-                </div>
-
-                <div className='flex items-center justify-center text-[.16rem] text-color-text2 cursor-pointer'>
-                  <div className='mx-[.06rem]'>
-                    {getShortAddress(item.poolAddress, 4)}
-                  </div>
-                </div>
-
-                <div className='flex items-center justify-center text-[.16rem] cursor-pointer'>
-                  <div className='mx-[.06rem]'>{item.statusSymbol}</div>
-                </div>
-
-                <div className='flex items-center justify-center text-[.16rem] text-color-text2 cursor-pointer'>
-                  <div className='mx-[.06rem]'>
-                    {' '}
-                    {moment
-                      .utc(item.timeStamp)
-                      .local()
-                      .format('D MMM YYYY h:mm a')}{' '}
-                  </div>
-                </div>
-
-                <div className='flex items-center justify-center text-[.16rem] text-color-text2 cursor-pointer'>
-                  <div className='mx-[.06rem]'>{item.status}</div>
-                </div>
-              </div>
-            ))}
+                ))
+            )}
           </div>
         )}
 
