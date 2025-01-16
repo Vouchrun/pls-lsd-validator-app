6ee26658a1168e78cc03116808f2eefd3d097389
ravirasadiya <ravirasadiya1994@gmail.com>
2025-01-16 13:00:54 +0530

    feat: update MyDataPubkeys component to include additional options for selection; replaced 30 with 50 and added 100 for improved user choice

--- components/myData/MyDataPubkeys.tsx
+++ components/myData/MyDataPubkeys.tsx
@@ -191,7 +191,8 @@ export const MyDataPubkeys = () => {
           >
             <option value={10}>10</option>
             <option value={20}>20</option>
-            <option value={30}>30</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
           </select>
         </div>
         <div className='text-[#FE8A3C] text-[14px] mx-[40px] md:my-0 my-[15px] flex'>
