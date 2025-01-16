7416f29b82b6c0fbf14515f543999fbdf3189d69
JexxaJ <44744647+JexxaJ@users.noreply.github.com>
2025-01-16 11:26:02 +0800

    version added

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -7,16 +7,23 @@ import appLogoLight from "public/images/logoStafi.svg";
 export default function Footer() {
     const { darkMode, unreadNoticeFlag } = useAppSlice();
   return (
-    <div className='w-smallContentW xl:w-contentW 2xl:w-largeContentW mx-auto'>
-        <a href='https://www.stafi.io/' target='_blank' className='flex items-center justify-end'>
-            <div style={{ color: darkMode ? "#fff" : "#000" }}>Powered by StaFi LSaaS</div>
-            <Image
-                src={darkMode ? appLogo : appLogoLight}
-                alt="stafi"
-                width={30}
-                className='ml-[5px]'
-            />
-        </a>
+    <div className='w-smallContentW xl:w-contentW 2xl:w-largeContentW mx-auto pb-1 flex flex-row justify-between items-center'>
+      <div style={{ color: darkMode ? '#fff' : '#000' }}>v1.0.1</div>
+      <a
+        href='https://www.stafi.io/'
+        target='_blank'
+        className='flex items-center justify-end'
+      >
+        <div style={{ color: darkMode ? '#fff' : '#000' }}>
+          Powered by StaFi LSaaS
+        </div>
+        <Image
+          src={darkMode ? appLogo : appLogoLight}
+          alt='stafi'
+          width={30}
+          className='ml-[5px]'
+        />
+      </a>
     </div>
   )
 }
