974b3fa4ae346a3c1fd89003f449ca322d0d6c15
JexxaJ <44744647+JexxaJ@users.noreply.github.com>
2025-09-10 10:54:56 +0800

    Increment version

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
   const { darkMode, unreadNoticeFlag } = useAppSlice();
   return (
     <div className='w-smallContentW xl:w-contentW 2xl:w-largeContentW mx-auto pb-1 flex flex-row justify-between items-center'>
-      <div style={{ color: darkMode ? '#fff' : '#000' }} className='text-[14px]'>v1.1.0</div>
+      <div style={{ color: darkMode ? '#fff' : '#000' }} className='text-[14px]'>v1.1.1</div>
       <a
         href='https://www.stafi.io/'
         target='_blank'
